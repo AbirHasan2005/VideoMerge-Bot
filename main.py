7dc99c54edddef34f5f5009a56edb25771c0e106
samadii <77415087+samadii@users.noreply.github.com>
2021-12-01 03:16:39 +0330

    Update main.py

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ async def videos_handler(bot: Client, m: Message):
     if (FormtDB.get(m.from_user.id, None) is not None) and (media.file_name.rsplit(".", 1)[-1].lower() != FormtDB.get(m.from_user.id)):
         await m.reply_text(f"First you sent a {FormtDB.get(m.from_user.id).upper()} video so now send only that type of video.", quote=True)
         return
-    input_ = f"{Config.DOWN_PATH}/{m.from_user.id}/input.txt"
-    if os.path.exists(input_):
+    dir = f"{Config.DOWN_PATH}/{m.from_user.id}/"
+    if os.path.exists(dir):
         await m.reply_text("Sorry Unkil,\nAlready One in Progress!\nDon't Spam Plox.")
         return
     isInGap, sleepTime = await CheckTimeGap(m.from_user.id)
@@ -200,7 +200,6 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
         duration = 0
         list_message_ids = QueueDB.get(cb.from_user.id, None)
         list_message_ids.sort()
-        input_ = f"{Config.DOWN_PATH}/{cb.from_user.id}/input.txt"
         if list_message_ids is None:
             await cb.answer("Queue Empty!", show_alert=True)
             await cb.message.delete(True)
@@ -235,6 +234,11 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
                         c_time
                     )
                 )
+                if file_dl_path.rsplit('.', 1)[1].lower() != "mp4":
+                    os.system(f"ffmpeg -i {file_dl_path} -c copy {file_dl_path.rsplit('.', 1)[0]}.mp4")
+                    file_dl_path = file_dl_path.rsplit('.', 1)[0] + ".mp4"
+                os.system(f"ffmpeg -i {file_dl_path} -c:v copy -bsf:v h264_mp4toannexb -c:a aac {file_dl_path.rsplit('.', 1)[0]}.ts")
+                file_dl_path = file_dl_path.rsplit('.', 1)[0] + ".ts"
             except Exception as downloadErr:
                 print(f"Failed to Download File!\nError: {downloadErr}")
                 QueueDB.get(cb.from_user.id).remove(i.message_id)
@@ -245,7 +249,7 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
             try:
                 if metadata.has("duration"):
                     duration += metadata.get('duration').seconds
-                vid_list.append(f"file '{file_dl_path}'")
+                vid_list.append(file_dl_path)
             except:
                 await delete_all(root=f"{Config.DOWN_PATH}/{cb.from_user.id}/")
                 QueueDB.update({cb.from_user.id: []})
@@ -256,15 +260,13 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
         for i in range(len(vid_list)):
             if vid_list[i] not in __cache:
                 __cache.append(vid_list[i])
-        vid_list = __cache
-        if (len(vid_list) < 2) and (len(vid_list) > 0):
+        vid_list = "|".join(__cache)
+        if (len(__cache) < 2) and (len(__cache) > 0):
             await cb.message.edit("There only One Video in Queue!\nMaybe you sent same video multiple times.")
             return
         await cb.message.edit("Trying to Merge Videos ...")
-        with open(input_, 'w') as _list:
-            _list.write("\n".join(vid_list))
         merged_vid_path = await MergeVideo(
-            input_file=input_,
+            vid_list=vid_list,
             user_id=cb.from_user.id,
             message=cb.message,
             format_=FormtDB.get(cb.from_user.id, "mkv")
