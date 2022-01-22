f649159ae261bbb61b7c32e9905b09ed7e3f5f00
fpband <82159763+fpband@users.noreply.github.com>
2022-01-22 13:32:22 +0330

    Update add_user.py

--- helpers/database/add_user.py
+++ helpers/database/add_user.py
@@ -12,5 +12,5 @@ async def AddUserToDatabase(bot: Client, cmd: Message):
         if Config.LOG_CHANNEL is not None:
             await bot.send_message(
                 int(Config.LOG_CHANNEL),
-                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
+                f"#کاربر_جدید: \n\nعضو جدید [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
             )
