25b2f2be85cd922f2c760ceccbf78c12f3a3d7c7
Jethalal-Pyro <106236965+Jethalal-Pyro@users.noreply.github.com>
2022-06-29 23:26:49 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -17,7 +17,7 @@ class Config(object):
     STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
     MONGODB_URI = os.environ.get("MONGODB_URI")
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", 2079695935)
+    BOT_OWNER = int(os.environ.get("BOT_OWNER",2079695935)
 
     START_TEXT = """
 Hi Unkil, I am Video Merge Bot!
